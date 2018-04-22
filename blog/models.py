711d9cb1f9b35b05ff821683ef8f3c1f64481aec
Cihan Erman <cihanerman@Cihan-MacBook-Pro.local>
2018-04-22 22:21:01 +0300

    translate gettext sorunu çözülemedi

--- blog/models.py
+++ blog/models.py
@@ -2,15 +2,17 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import uggetext_lazy as _
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,verbose_name=_('author'),
+    ))
+    title = models.CharField(_('title'), max_length=200)
+    text = models.TextField(_('text'),)
+    created_date = models.DateTimeField(_('Created Date'),
             default=timezone.now)
-    published_date = models.DateTimeField(
+    published_date = models.DateTimeField(_('Published Date'),
             blank=True, auto_now_add=True)
 
     def publish(self):
--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -104,6 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
+from django.utils.translation import ugettext_lazy as _
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"))
+
+LANGUAGES = (
+    ('tr', _('Turkish')),
+    ('en', _('English')),
+)
 
 LANGUAGE_CODE = 'en-us'
 
